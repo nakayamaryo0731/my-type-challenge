ed893d24e5042acff82531c3df8bc1822a80834f
nakayamaryo0731 <ryo_nakayama@caddi.jp>
2023-05-21 15:00:50 +0900

    complete easy

---
+++ easy/189-Awaited.ts
@@ -0,0 +1,31 @@
+type Thenable<T> = {
+  then: (onfulfilled: (arg: T) => unknown) => unknown;
+}
+
+type MyAwaited<T extends  Thenable<any> | Promise<any>> = T extends Promise<infer U> 
+? U extends Promise<any>
+  ? MyAwaited<U>
+  : U
+: T extends Thenable<infer U> 
+  ? U 
+  : never
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type X = Promise<string>
+type Y = Promise<{ field: number }>
+type Z = Promise<Promise<string | number>>
+type Z1 = Promise<Promise<Promise<string | boolean>>>
+type T = { then: (onfulfilled: (arg: number) => any) => any }
+
+type cases = [
+  Expect<Equal<MyAwaited<X>, string>>,
+  Expect<Equal<MyAwaited<Y>, { field: number }>>,
+  Expect<Equal<MyAwaited<Z>, string | number>>,
+  Expect<Equal<MyAwaited<Z1>, string | boolean>>,
+  Expect<Equal<MyAwaited<T>, number>>,
+]
+
+// @ts-expect-error
+type error = MyAwaited<number>
---
+++ easy/533-Concat.ts
@@ -0,0 +1,20 @@
+/* _____________ Your Code Here _____________ */
+
+type Arr = readonly any[];
+type Concat<T extends Arr, U extends Arr> = [...T, ...U];
+
+/* _____________ Test Cases _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+const tuple = [1] as const
+
+type cases = [
+  Expect<Equal<Concat<[], []>, []>>,
+  Expect<Equal<Concat<[], [1]>, [1]>>,
+  Expect<Equal<Concat<typeof tuple, typeof tuple>, [1, 1]>>,
+  Expect<Equal<Concat<[1, 2], [3, 4]>, [1, 2, 3, 4]>>,
+  Expect<Equal<Concat<['1', 2, '3'], [false, boolean, '4']>, ['1', 2, '3', false, boolean, '4']>>,
+]
+
+// @ts-expect-error
+type error = Concat<null, undefined>
